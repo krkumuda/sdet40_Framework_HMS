b79ed5c021b862b750497dcb499ff0fa98e0086b
krkum <krkum@KUMUDA>
2022-09-21 10:19:15 +0530

    in AddDoctorPage had some changes

--- src/main/java/org/HMS/objectRepository/AddDoctorPage.java
+++ src/main/java/org/HMS/objectRepository/AddDoctorPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+// some changes had done for github
 public class AddDoctorPage {
 	public  AddDoctorPage(WebDriver driver) {
 		PageFactory.initElements(driver, this);
